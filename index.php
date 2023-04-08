da5cfb57e6b40163af1537b08d17464b8ba6d4dd
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2023-04-08 02:25:53 -0700

    added css to index page

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <?php
 
+
+
+
 session_start();
 
 if (isset($_SESSION["currentID"])) {
     $mysqli = require __DIR__ . "/database.php";
 
     $sql = "SELECT * FROM account WHERE id = {$_SESSION["currentID"]}";
-
     $result = $mysqli->query($sql);
-
     $user = $result->fetch_assoc();
+
+
+    $sql = "SELECT * FROM preference WHERE acct_id = {$_SESSION["currentID"]}";
+    $result2 = $mysqli->query($sql);
+    $userPreferences = $result2->fetch_assoc();
 }
 ?>
 
+
+
+
 <html>
+
+
+
 
 <head>
     <title>User</title>
     <link rel="stylesheet" href="styles/index.css">
 </head>
+
+
+
 
 <body>
     <!-- <a href="home.html"><img src="ITCLogoOutline.png" class="logo"></a>
@@ -33,10 +48,19 @@ if (isset($_SESSION["currentID"])) {
         </div>
     </div> -->
 
+
+
+
     <a href="home.html"><img src="ITCLogoOutline.png" class="logo"></a>
     <hr />
     <div class="parent">
-        <a href="search.php"><button class="topleft searchButton">Search</button> </a>
+        <div class="topleft">
+            <a href="search.php"><button class="searchButton">Search</button> </a>
+        </div>
+
+
+
+
         <div class="topright">
             <a href="moreInfo.php"><button class="acctButton">Edit Account</button></a>
             <a href="home.html"><button class="acctButton">Sign Out</button></a>
@@ -45,7 +69,44 @@ if (isset($_SESSION["currentID"])) {
             <p class="centertext">Hello <?= $user["firstName"] ?>!</p>
         </div>
 
-    </div>
-</body>
 
+
+
+        <div class="bioDiv">
+            <h1 class="bioHeader">Your Biography</h1>
+            <textarea disabled class="bioText" name="desc" id="desc" cols="30" rows="10"><?=$user["bio"] ?></textarea>
+
+
+            <p class="universityP">University: <?= $user["university"] ?></p>
+            <p class="majorP">Major: <?= $user["major"] ?></p>
+        </div>
+
+
+        <div class="preferenceDiv">
+            <div class = "leftFlexBox">
+            <div class="preferenceBox">Cleanliness: <?= $userPreferences["clean"] ?></div>
+            <div class="preferenceBox">Pets:  <?= $userPreferences["petPreference"] ?></div>
+            <div class="preferenceBox">Smoking: <?= $userPreferences["smoke"] ?></div>
+            <div class="preferenceBox">Drugs: <?= $userPreferences["drugs"] ?></div>
+            <div class="preferenceBox">Weekday Sleep: <?= $userPreferences["weekdaySleep"] ?></div>
+
+
+            </div>
+            <div class = "rightFlexBox">
+            <div class="preferenceBox">Weekend Sleep: <?= $userPreferences["weekendSleep"] ?></div>
+            <div class="preferenceBox">Noise: <?=$userPreferences["loud"] ?></div>
+            <div class="preferenceBox"></div>
+            <div class="preferenceBox"></div>
+            <div class="preferenceBox"></div>
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
 </html>
+
+
+
+
