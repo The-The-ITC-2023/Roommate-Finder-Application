c6f08fefcf3a111cd9172059ff8a73bf6d1000ef
alonzojp <alonzojp@gmail.com>
2023-04-15 00:45:46 -0700

    Fixed unavailable image

--- picture.php
+++ picture.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 
@@ -11,21 +11,21 @@ if (isset($_SESSION["currentID"])) {
     $result1 = $mysqli->query($sql);
     $result = $result1->fetch_assoc();
 }
- 
+
 // If file upload form is submitted 
-$status = $statusMsg = ''; 
-if(isset($_POST["submit"])){ 
-    $status = 'error'; 
-    if(!empty($_FILES["image"]["name"])) { 
+$status = $statusMsg = '';
+if (isset($_POST["submit"])) {
+    $status = 'error';
+    if (!empty($_FILES["image"]["name"])) {
         // Get file info 
-        $fileName = basename($_FILES["image"]["name"]); 
-        $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
-         
+        $fileName = basename($_FILES["image"]["name"]);
+        $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
+
         // Allow certain file formats 
-        $allowTypes = array('jpg','png','jpeg'); 
-        if(in_array($fileType, $allowTypes)){ 
-            $image = $_FILES['image']['tmp_name']; 
-            $imgContent = addslashes(file_get_contents($image)); 
+        $allowTypes = array('jpg', 'png', 'jpeg');
+        if (in_array($fileType, $allowTypes)) {
+            $image = $_FILES['image']['tmp_name'];
+            $imgContent = addslashes(file_get_contents($image));
 
             $mysqli = require __DIR__ . "/database.php";
 
@@ -37,36 +37,38 @@ if(isset($_POST["submit"])){
             }
             $stmt4->execute();
             header("Location: index.php");
-
-            
         }
     }
-} 
- 
+}
+
 // Display status message 
-echo $statusMsg; 
+echo $statusMsg;
 ?>
 
 <html>
-    <head>
+
+<head>
     <link rel="stylesheet" href="styles/picture.css">
 
-    </head>
-    <body>
-        <img src="ITCLogoOutline.png" class="logo">
-        <hr>
-        <div class="photo">
-            <div id="title">Last Step: Add Profile Photo</div>
-            <form action="picture.php" method="post" enctype="multipart/form-data" class="photo-form">
-                <label id="image-header">Select Your Profile Image:</label>
-                <input type="file" name="image" id="file-select">
-                <img src="ImageError.png" id="profile">
-                <div id="warning">*Only file types .jpg .png & .jpeg are accepted</div>
-                <input type="submit" name="submit" value="Confirm Photo" id="upload-button" class="button">
-            </form>
-            <div class="submit-button">
-                <a href="index.php"><button class="button" <?php if ($result['image'] == null) {echo "disabled";} ?> >Finish Setting Profile</button></a>
-            </div>
+</head>
+
+<body>
+    <img src="ITCLogoOutline.png" class="logo">
+    <hr>
+    <div class="photo">
+        <div id="title">Last Step: Add Profile Photo</div>
+        <form action="picture.php" method="post" enctype="multipart/form-data" class="photo-form">
+            <label id="image-header">Select Your Profile Image:</label>
+            <input type="file" name="image" id="file-select">
+            <div id="warning">*Only file types .jpg .png & .jpeg are accepted</div>
+            <input type="submit" name="submit" value="Confirm Photo" id="upload-button" class="button">
+        </form>
+        <div class="submit-button">
+            <a href="index.php"><button class="button" <?php if ($result['image'] == null) {
+                                                            echo "hidden";
+                                                        } ?>>Finish Setting Profile</button></a>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
